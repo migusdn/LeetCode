b41b95fd3f8927bea75d4241cda71d7f2c5134de
HYUNWOO PARK <44918187+migusdn@users.noreply.github.com>
2024-05-10 03:28:24 +0900

    Time: 1 ms (92.59%), Space: 45.9 MB (30.09%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int max = 0;
-        int start = prices[0];
-        int len = prices.length;
-        for(int i = 1;i<len; i++){
-            if(start < prices[i]) max += prices[i] - start;
-            start = prices[i];
+        int min = prices[0];
+        int leng = prices.length;
+        for(int i = 1;i<leng; i++){
+            if(min < prices[i]) max += prices[i] - min;
+            min = prices[i];
         }
         return max;
     }
