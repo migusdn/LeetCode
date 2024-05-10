cbf27b6e828d31551841e492adb3428822fade2b
HYUNWOO PARK <44918187+migusdn@users.noreply.github.com>
2024-05-10 19:36:54 +0900

    Time: 0 ms (100%), Space: 41.4 MB (84.11%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s.trim();
-        String[] temp = s.split(" ");
-        return temp[temp.length-1].length();
+        int i=s.length()-1;
+        int ans=0;
+        while(i>=0&&s.charAt(i)==' ')
+            i--;
+        while(i>=0&&s.charAt(i)!=' '){
+            i--;
+            ans++;
+        }
+        return ans;
     }
 }
